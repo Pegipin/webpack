12fe4102810734a08bdc27f1769e00b7735ec2c3
Pegipin <pegah.chenna@gmail.com>
2021-08-18 13:41:49 +0100

    font added style.css added print.js added picture added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 
 import _ from 'lodash';
+import './style.css';
+import Icon from './winnie1.png';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+      btn.onclick = printMe;
+
+          element.appendChild(btn);
+
+
+    const myIcon = new Image();
+      myIcon.src = Icon;
+
+          element.appendChild(myIcon);
+    console.log(Data);
+      console.log(Notes);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+            index: './src/index.js',
+        print: './src/print.js',
+      },
+
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    module: {
+            rules: [
+          {
+            test: /\.css$/i,
+           use: ['style-loader', 'css-loader'],
+          },
+                {
+                        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+         },
+                {
+                        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+                {
+                        test: /\.(csv|tsv)$/i,
+            use: ['csv-loader'],
+         },
+      {
+        test: /\.xml$/i,
+           use: ['xml-loader'],
+         },
+
+
+    ],
+  },
 };
